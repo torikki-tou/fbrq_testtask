0542542739bc5ea0fb9fcfd85308c1885989fb13
torikki <kzymm1@gmail.com>
2022-12-03 10:58:22 +0300

    base class for crud operations added

---
+++ src/repo/__init__.py
@@ -0,0 +1,1 @@
+from src.repo.client import client
---
+++ src/repo/base.py
@@ -0,0 +1,70 @@
+from typing import TypeVar, Generic, Type, Optional
+
+from bson import ObjectId
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
+
+Model = TypeVar('Model', bound=BaseModel)
+CreateSchema = TypeVar('CreateSchema', bound=BaseModel)
+UpdateSchema = TypeVar('UpdateSchema', bound=BaseModel)
+
+
+class BaseRepo(Generic[Model, CreateSchema, UpdateSchema]):
+    def __init__(self, model: Type[Model], database: str, collection: str):
+        self.model = model
+        self.database = database
+        self.collection = collection
+    
+    def get_db_collection(
+            self, db_client: AsyncIOMotorClient
+    ) -> AsyncIOMotorCollection:
+        return db_client[self.database][self.collection]
+
+    async def get(
+            self,
+            db_client: AsyncIOMotorClient,
+            id_: str | ObjectId
+    ) -> Optional[Model]:
+        if not isinstance(id_, ObjectId) and not ObjectId.is_valid(id_):
+            return None
+        collection = self.get_db_collection(db_client)
+        obj = await collection.find_one({'_id': ObjectId(id_)})
+        if not obj:
+            return None
+        obj['id'] = str(obj.pop('_id'))
+        return self.model(**obj)
+
+    async def create(
+            self,
+            db_client: AsyncIOMotorClient,
+            obj_in: CreateSchema
+    ) -> Model:
+        collection = self.get_db_collection(db_client)
+        obj_id = (await collection.insert_one(obj_in)).inserted_id
+        return await self.get(collection, id_=obj_id)
+
+    async def update(
+            self,
+            db_client: AsyncIOMotorClient,
+            id_: str | ObjectId,
+            obj_in: UpdateSchema
+    ) -> Optional[Model]:
+        if not isinstance(id_, ObjectId) and not ObjectId.is_valid(id_):
+            return None
+        collection = self.get_db_collection(db_client)
+        await collection.update_one({'_id': ObjectId(id_)}, {'$set': obj_in})
+        return await self.get(collection, id_=id_)
+
+    async def remove(
+            self,
+            db_client: AsyncIOMotorClient,
+            id_: str | ObjectId
+    ) -> Optional[Model]:
+        if not isinstance(id_, ObjectId) and not ObjectId.is_valid(id_):
+            return None
+        collection = self.get_db_collection(db_client)
+        obj = await self.get(collection, id_=id_)
+        if not obj:
+            return None
+        await collection.delete_one({'_id': ObjectId(id_)})
+        return self.model(**obj.dict())
