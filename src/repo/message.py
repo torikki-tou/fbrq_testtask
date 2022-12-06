9a53f3f33925458a1110fd42a35e16f67a4429ac
torikki <kzymm1@gmail.com>
2022-12-06 18:41:59 +0300

    count by status added

--- src/repo/message.py
+++ src/repo/message.py
@@ -1,9 +1,21 @@
+from bson import ObjectId
+from pymongo import MongoClient
+
 from src.repo.base import BaseRepo
-from src.schemas import Message, MessageCreate, MessageUpdate
+from src.schemas import Message, MessageCreate, MessageUpdate, MessageStatus
 
 
 class MessageRepo(BaseRepo[Message, MessageCreate, MessageUpdate]):
-    pass
+    def count_by_mailing_and_status(
+            self,
+            db_client: MongoClient,
+            mailing_id: str | ObjectId,
+            status: MessageStatus
+    ) -> int:
+        collection = self.get_db_collection(db_client)
+        return collection.count_documents(
+            {'mailing_id': str(mailing_id), 'status': status.value}
+        )
 
 
 message = MessageRepo(Message, 'api', 'message')
