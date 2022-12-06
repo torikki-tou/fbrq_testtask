a82215053733ef0bf5b8f1b75c3ad9ef3f6a4d99
torikki <kzymm1@gmail.com>
2022-12-06 18:41:50 +0300

    get_multi added

--- src/repo/mailing.py
+++ src/repo/mailing.py
@@ -1,9 +1,23 @@
+from typing import Iterable
+
+from pymongo import MongoClient
+
 from src.repo.base import BaseRepo
 from src.schemas import Mailing, MailingCreate, MailingUpdate
 
 
 class MailingRepo(BaseRepo[Mailing, MailingCreate, MailingUpdate]):
-    pass
+    def get_multi(
+            self,
+            db_client: MongoClient,
+            limit: int = 10,
+            offset: int = 0
+    ) -> Iterable:
+        collection = self.get_db_collection(db_client)
+        return map(
+            lambda obj: Mailing(**obj),
+            collection.find(skip=offset).limit(limit)
+        )
 
 
 mailing = MailingRepo(Mailing, 'api', 'mailing')
