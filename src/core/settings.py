ef654d50de216b470358e811cacdafa9dd52686b
torikki <kzymm1@gmail.com>
2022-12-06 18:42:11 +0300

    settings updated

--- src/core/settings.py
+++ src/core/settings.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     MONGO_CONNECTION_STRING: str
     CELERY_BROKER_CON_STRING: str
+    PROBE_SERVICE_JWT: str
 
 
 settings = Settings()
